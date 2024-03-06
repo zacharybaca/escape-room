66a2af05afdacb77bfba6bd7f265d9c0721898cb
Zachary Baca <zachary.baca@msn.com>
2024-03-06 08:33:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Import The Readline Module From Node
 const readLine = require('readline-sync');
+
+/*
+Tell the user their options:
+
+1. Find the key, or
+2. Put hand in hole
+3. Open the door
+*/
+
+console.log("Welcome to the Escape Room: Your options are, Find the key, Put hand in hold or Open the door");
